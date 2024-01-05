d2645c44ed70bf43514cd6c4ac376eabaeb4d3fb
Arun Kumar Nama <arun.nama@gmail.com>
2024-01-05 23:32:14 +0100

    linkedlistchanges

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+class LinkedListNode<T> {
+    var value: T?
+    var next: LinkedListNode?
+    weak var previousNode: LinkedListNode?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
+
+
